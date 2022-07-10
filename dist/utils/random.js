eea8fe54a859a13b18d7d90def668f233ff0665c
kota-yata <kota@yatagai.com>
2022-07-11 01:39:33 +0900

    WIP: power func

--- dist/utils/random.js
+++
@@ -1,6 +0,0 @@
-export const randomInt = (min, max) => {
-    const minForFloor = min + 1;
-    const base = Math.random();
-    const num = Math.floor(base * (max - minForFloor) + minForFloor);
-    return num;
-};
